305dea4da396927a62f18dff2074730e316057b8
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-02-24 17:12:37 -0600

    best css structure for globals utilities

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -31,190 +31,189 @@ const Nosotros = () => {
   if (error) return <p>Oh no... {error.message}</p>;
 
   return (
-    <div class="margin-top-100">
-      <section class="full-row about-company">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8">
-              <div
-                class="wow fadeInLeft"
-                data-wow-delay="300ms"
-                data-wow-duration="700ms"
-              >
-                <h2 class="title">
-                  <span>Bienvenido </span>{" "}
-                  {
-                    result.data.nosotrossConnection.edges[0].node
-                      .tituloPrincipal
-                  }
-                </h2>
-                <span class="sub-title">
-                  {" "}
-                  {result.data.nosotrossConnection.edges[0].node.subtituloAbout}
-                </span>
-                <div class="text-area">
-                  {
-                    result.data.nosotrossConnection.edges[0].node
-                      .subtituloAbout2
-                  }
-                </div>
+    <section class="full-row about-company margin-top-100">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div
+              class="wow fadeInLeft"
+              data-wow-delay="300ms"
+              data-wow-duration="700ms"
+            >
+              <h2 class="title wow">
+                <span>Bienvenido </span>
+                {result.data.nosotrossConnection.edges[0].node.tituloPrincipal}
+              </h2>
+              <span class="sub-title">
+                {" "}
+                {result.data.nosotrossConnection.edges[0].node.subtituloAbout}
+              </span>
+              <div class="text-area">
+                {result.data.nosotrossConnection.edges[0].node.subtituloAbout2}
               </div>
-              <div class="row">
-                <div class="fact-counter">
-                  <div class="col-lg-3 col-sm-3 col-xs-6">
+            </div>
+
+            {/* Test for wow */}
+            <div
+              class="wow fadeInLeft"
+              data-wow-delay="300ms"
+              data-wow-duration="700ms"
+            >
+              <h2 class="title wow">Lorem wow test</h2>
+            </div>
+            <div class="row">
+              <div class="fact-counter">
+                <div class="col-lg-3 col-sm-3 col-xs-6">
+                  <div
+                    class="count color-dark wow fadeIn"
+                    data-wow-delay="300ms"
+                    data-wow-duration="700ms"
+                  >
+                    <span class="flat-icon flaticon-line-chart color-default"></span>
                     <div
-                      class="count color-dark wow fadeIn"
-                      data-wow-delay="300ms"
-                      data-wow-duration="700ms"
+                      class="amount count-num"
+                      data-speed="3000"
+                      data-stop="230"
                     >
-                      <span class="flat-icon flaticon-line-chart color-default"></span>
-                      <div
-                        class="amount count-num"
-                        data-speed="3000"
-                        data-stop="230"
-                      >
-                        10
-                      </div>
-                      <span>Años de Experiencia</span>
+                      10
                     </div>
+                    <span>Años de Experiencia</span>
                   </div>
-                  <div class="col-lg-3 col-sm-3 col-xs-6">
+                </div>
+                <div class="col-lg-3 col-sm-3 col-xs-6">
+                  <div
+                    class="count color-dark wow fadeIn"
+                    data-wow-delay="300ms"
+                    data-wow-duration="700ms"
+                  >
+                    <span class="flat-icon flaticon-user-1 color-default"></span>
                     <div
-                      class="count color-dark wow fadeIn"
-                      data-wow-delay="300ms"
-                      data-wow-duration="700ms"
+                      class="amount count-num"
+                      data-speed="3000"
+                      data-stop="141"
                     >
-                      <span class="flat-icon flaticon-user-1 color-default"></span>
-                      <div
-                        class="amount count-num"
-                        data-speed="3000"
-                        data-stop="141"
-                      >
-                        +50
-                      </div>
-                      <span>Productos Quultiva</span>
+                      +50
                     </div>
+                    <span>Productos Quultiva</span>
                   </div>
-                  <div class="col-lg-3 col-sm-3 col-xs-6">
+                </div>
+                <div class="col-lg-3 col-sm-3 col-xs-6">
+                  <div
+                    class="count color-dark wow fadeIn"
+                    data-wow-delay="300ms"
+                    data-wow-duration="700ms"
+                  >
+                    <span class="flat-icon flaticon-nature-3 color-default"></span>
                     <div
-                      class="count color-dark wow fadeIn"
-                      data-wow-delay="300ms"
-                      data-wow-duration="700ms"
+                      class="amount count-num"
+                      data-speed="3000"
+                      data-stop="172"
                     >
-                      <span class="flat-icon flaticon-nature-3 color-default"></span>
-                      <div
-                        class="amount count-num"
-                        data-speed="3000"
-                        data-stop="172"
-                      >
-                        +10
-                      </div>
-                      <span>Diseños Originales</span>
+                      +10
                     </div>
+                    <span>Diseños Originales</span>
                   </div>
-                  <div class="col-lg-3 col-sm-3 col-xs-6">
+                </div>
+                <div class="col-lg-3 col-sm-3 col-xs-6">
+                  <div
+                    class="count color-dark wow fadeIn"
+                    data-wow-delay="300ms"
+                    data-wow-duration="700ms"
+                  >
+                    <span class="flat-icon flaticon-time color-default"></span>
                     <div
-                      class="count color-dark wow fadeIn"
-                      data-wow-delay="300ms"
-                      data-wow-duration="700ms"
+                      class="amount count-num"
+                      data-speed="3000"
+                      data-stop="2347"
                     >
-                      <span class="flat-icon flaticon-time color-default"></span>
-                      <div
-                        class="amount count-num"
-                        data-speed="3000"
-                        data-stop="2347"
-                      >
-                        +20
-                      </div>
-                      <span>
-                        Proveedores que garantizan suministro de productos
-                      </span>
+                      +20
                     </div>
+                    <span>
+                      Proveedores que garantizan suministro de productos
+                    </span>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col-md-4">
+          </div>
+          <div class="col-md-4">
+            <img
+              class="none-mobile"
+              src={result.data.nosotrossConnection.edges[5].node.soloImagen.url}
+              alt=""
+            />
+          </div>
+        </div>
+
+        {/* ICONOS DIVIDER */}
+        <div class="row">
+          <div class="col-md-4 col-sm-4">
+            <div class="dark-gradient about-link">
               <img
-                class="none-mobile"
                 src={
-                  result.data.nosotrossConnection.edges[5].node.soloImagen.url
+                  result.data.nosotrossConnection.edges[4].node.soloImagen.url
                 }
                 alt=""
               />
+              <div class="overlay">
+                <h5 class="inner-title">
+                  {" "}
+                  {
+                    result.data.nosotrossConnection.edges[4].node
+                      .tituloPrincipal
+                  }
+                </h5>
+                <a href="#" class="btn-link">
+                  Leer más
+                </a>
+              </div>
             </div>
           </div>
-
-          {/* ICONOS DIVIDER */}
-          <div class="row">
-            <div class="col-md-4 col-sm-4">
-              <div class="dark-gradient about-link">
-                <img
-                  src={
-                    result.data.nosotrossConnection.edges[4].node.soloImagen.url
+          <div class="col-md-4 col-sm-4">
+            <div class="dark-gradient about-link">
+              <img
+                src={
+                  result.data.nosotrossConnection.edges[2].node.soloImagen.url
+                }
+                alt="sdscsf"
+              />
+              <div class="overlay">
+                <h5 class="inner-title">
+                  {
+                    result.data.nosotrossConnection.edges[2].node
+                      .tituloPrincipal
                   }
-                  alt=""
-                />
-                <div class="overlay">
-                  <h5 class="inner-title">
-                    {" "}
-                    {
-                      result.data.nosotrossConnection.edges[4].node
-                        .tituloPrincipal
-                    }
-                  </h5>
-                  <a href="#" class="btn-link">
-                    Leer más
-                  </a>
-                </div>
+                </h5>
+                <a href="#" class="btn-link">
+                  Leer más
+                </a>
               </div>
             </div>
-            <div class="col-md-4 col-sm-4">
-              <div class="dark-gradient about-link">
-                <img
-                  src={
-                    result.data.nosotrossConnection.edges[2].node.soloImagen.url
+          </div>
+          <div class="col-md-4 col-sm-4">
+            <div class="dark-gradient about-link">
+              <img
+                src={
+                  result.data.nosotrossConnection.edges[3].node.soloImagen.url
+                }
+                alt="big"
+              />
+              <div class="overlay">
+                <h5 class="inner-title">
+                  {
+                    result.data.nosotrossConnection.edges[3].node
+                      .tituloPrincipal
                   }
-                  alt="sdscsf"
-                />
-                <div class="overlay">
-                  <h5 class="inner-title">
-                    {
-                      result.data.nosotrossConnection.edges[2].node
-                        .tituloPrincipal
-                    }
-                  </h5>
-                  <a href="#" class="btn-link">
-                    Leer más
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-              <div class="dark-gradient about-link">
-                <img
-                  src={
-                    result.data.nosotrossConnection.edges[3].node.soloImagen.url
-                  }
-                  alt="big"
-                />
-                <div class="overlay">
-                  <h5 class="inner-title">
-                    {
-                      result.data.nosotrossConnection.edges[3].node
-                        .tituloPrincipal
-                    }
-                  </h5>
-                  <a href="#" class="btn-link">
-                    Saber más
-                  </a>
-                </div>
+                </h5>
+                <a href="#" class="btn-link">
+                  Saber más
+                </a>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,20 +3,7 @@ import * as ReactDOM from "react-dom";
 import Navbar from "./components/NavBar/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-import "../src/css/bootstrap.css";
-import "../src/css/color.css";
-import "../src/css/animate.min.css";
-import "../src/css/bg-color-settings.css";
-import "../src/css/default-animation.css";
-import "../src/css/extralayers.css";
-import "../src/css/font-awesome.min.css";
-import "../src/css/jquery.fancybox.css";
-import "../src/css/loader.css";
-import "../src/css/owl.css";
-import "../src/css/responsive.css";
-import "../src/css/settings.css";
-import "../src/css/style.css";
+import "../src/index.css";
 import Preloader from "./components/Preloader/Preloader";
 import { createClient, Provider } from "urql";
 
