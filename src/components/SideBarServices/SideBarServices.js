e61c6d2ea71067db18cfece699c0d72cc6ebb976
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-02-28 19:58:07 -0600

    add a map function for side bar pages and implement slider component

--- src/components/SideBarServices/SideBarServices.js
+++ src/components/SideBarServices/SideBarServices.js
@@ -1,82 +1,31 @@
 import React from "react";
 
-const SideBarServices = (props) => {
+const SideBarServices = () => {
+  const Productos = [
+    { id: 1, name: "Huertos" },
+    { id: 2, name: "Plagas" },
+    { id: 3, name: "Riego Insumos" },
+    { id: 4, name: "Fertilizantes" },
+    { id: 5, name: "Cultivo" },
+    { id: 6, name: "Biogreen" },
+    { id: 7, name: "Invernaderos Mediana Escala" },
+    { id: 8, name: "Invernaderos Escala Comercial" },
+    { id: 9, name: "Accesorios para invernadero" },
+  ];
   return (
     <div class="col-md-3">
       <div class="service-sidebar">
         <ul class="srvc-ctgry">
-          <li class={props.active}>
-            <a>
-              <span>
-                <i class="flaticon-nature-1"></i>
-              </span>
-              Huertos
-            </a>
-          </li>
-          <li>
-            <a href="garden-care.html">
-              <span>
-                <i class="flaticon-travel"></i>
-              </span>
-              Plagas
-            </a>
-          </li>
-          <li>
-            <a href="watering-garden.html">
-              <span>
-                <i class="flaticon-nature-1"></i>
-              </span>
-              Riego Insumos
-            </a>
-          </li>
-          <li>
-            <a href="garden-design.html">
-              <span>
-                <i class="flaticon-plant"></i>
-              </span>
-              Fertilizantes
-            </a>
-          </li>
-          <li>
-            <a href="gutter-repair.html">
-              <span>
-                <i class="flaticon-fence"></i>
-              </span>
-              Cultivo
-            </a>
-          </li>
-          <li>
-            <a href="lawn-care.html">
-              <span>
-                <i class="flaticon-machine"></i>
-              </span>
-              Biogreen
-            </a>
-          </li>
-          <li>
-            <a href="land-design.html">
-              <span>
-                <i class="flaticon-tool"></i>
-              </span>
-              Invernaderos Mediana Escala
-            </a>
-          </li>
-          <li>
-            <a href="agricultural-research.html">
-              <span>
-                <i class="flaticon-greenhouse"></i>
-              </span>
-              Invernaderos Escala Comercial
-            </a>
-          </li>
-          <li>
-            <a href="agricultural-research.html">
-              <span>
-                <i class="flaticon-greenhouse"></i>
-              </span>
-              Accesorios para invernadero
-            </a>
-          </li>
+          {Productos.map((id, name) => (
+            <li key={name}>
+              <a href="garden-care.html">
+                <span>
+                  <i class="flaticon-travel"></i>
+                </span>
+                {id.name}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/pages/huertos/huertos.js
+++ src/pages/huertos/huertos.js
@@ -1,55 +1,27 @@
 import React, { useEffect } from "react";
 import PageBanner from "../../components/PageBanner/PageBanner";
 import SideBarServices from "../../components/SideBarServices/SideBarServices";
-import $ from "jquery";
-
+import Slider from "../../components/Slider/Slider";
 const Huertos = () => {
   return (
-    <section>
+    <div>
       <PageBanner title="huertos" inicio="inicio" />
-      <section class="full-row">
+      <div class="full-row">
         <div class="container">
-          <div class="row">
-            <SideBarServices class={active} />
-            <div class="col-md-9">
-              <div class="service-details">
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="service-overview">
-                      <div class="service-slider">
-                        <div class="irem">
-                          <img
-                            src="https://i.imgur.com/jq76ja5.png"
-                            alt="..."
-                          />
-                        </div>
-                        <div class="irem">
-                          <img
-                            src="https://i.imgur.com/jq76ja5.png"
-                            alt="..."
-                          />
-                        </div>
-                        <div class="irem">
-                          <img
-                            src="https://i.imgur.com/jq76ja5.png"
-                            alt="..."
-                          />
-                        </div>
-                      </div>
-                      <div class="overview-caption">
-                        <h4 class="inner-title">
-                          <span>Quultiva</span> Huertos
-                        </h4>
-                        <p>
-                          Interdum cum accumsan bibendum nullam justo elementum
-                          semper. Maecenas ultrices elit arcu ante sapien.
-                          Sollicitudin orci laoreet. Dui suscipit justo facilisi
-                          eget Sapien curabitur massa purus primis, scelerisque
-                          elementum in Porta Netus risus et mus quam ligula
-                          tempus sociis molestie hymenaeos cras interdum class
-                          hac lectus pulvinar morbi amet vestibulum.
-                        </p>
-                      </div>
+          <SideBarServices />
+
+          <div class="col-md-9">
+            <div class="service-details">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="service-overview">
+                    <div class="service-slider">
+                      <Slider />
+                    </div>
+                    <div class="overview-caption">
+                      <h4 class="inner-title">
+                        <span>Quultiva</span> Huertos
+                      </h4>
                     </div>
                   </div>
                 </div>
@@ -57,8 +29,8 @@ const Huertos = () => {
             </div>
           </div>
         </div>
-      </section>
-    </section>
+      </div>
+    </div>
   );
 };
 
